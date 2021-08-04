f3cf5ac3741a9be6d7298673a71b50cbcf03d937
Arthur Andrade <arthur.diegoo@hotmail.com>
2021-08-04 20:02:22 -0300

    feat: add role-based authorization (admin and mods)

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,6 +1,9 @@
 import { Config } from "../interfaces"
+import { getAdminRole, getModRoles } from "./roleMapper"
 
 export const globalConfig: Config = {
   token: process.env.DISCORD_BOT_API_KEY,
   prefix: process.env.DISCORD_BOT_PREFIX,
+  adminRole: getAdminRole(),
+  modRoles: getModRoles()
 }
